a6d54236c140013bf9dfe3cae0c8916320598c09
Александр <pyankov.work@gmail.com>
2019-11-06 16:24:35 +0300

    Add Polygon figure and test for it GetArea method

---
+++ GeometryMaster/Environment/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryMaster
+{
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ GeometryMaster/Evklid/Polygon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryMaster.Evklid
+{
+    public class Polygon : FlatFigure
+    {
+        private Point[] points;
+
+        public Polygon(IEnumerable<Point> points) : this(points.ToArray()) { }
+        public Polygon(params Point[] points)
+        {
+            this.points = points;
+        }
+
+        public override double GetArea()
+        {
+            Func<int, int, int> getLeftIndex = (current, max) => current - 1 < 0 ? max - 1 : current - 1;
+            Func<int, int, int> getRightIndex = (current, max) => (current + 1) % max;
+
+            return 0.5 * points
+                .Select((p, i) => new {
+                    p.X,
+                    Y1 = points[getLeftIndex(i, points.Length)].Y,
+                    Y2 = points[getRightIndex(i, points.Length)].Y
+                })
+                .Sum(p => p.X * (p.Y1 - p.Y2));
+        }
+    }
+}
