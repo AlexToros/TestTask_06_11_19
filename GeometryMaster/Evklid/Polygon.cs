c92b6cf2e2301bf9afe70c3cf21d9f0a33493d26
Александр <pyankov.work@gmail.com>
2019-11-07 08:54:57 +0300

    Add Exersize explanation

--- GeometryMaster/Evklid/Polygon.cs
+++ GeometryMaster/Evklid/Polygon.cs
@@ -28,6 +28,7 @@ namespace GeometryMaster.Evklid
             this.points = points;
         }
 
+        // Метод, реализующий вычисление площади для фигур, построенных по точкам, независящий от типа фигуры
         // https://ru.wikipedia.org/wiki/%D0%A4%D0%BE%D1%80%D0%BC%D1%83%D0%BB%D0%B0_%D0%BF%D0%BB%D0%BE%D1%89%D0%B0%D0%B4%D0%B8_%D0%93%D0%B0%D1%83%D1%81%D1%81%D0%B0
         /// <summary>
         /// Вычисление площади на основе метода Гаусса 
