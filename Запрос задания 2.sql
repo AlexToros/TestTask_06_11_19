fed060ddbeb11f1900bd0dc43f6368f522e5c38d
Александр <pyankov.work@gmail.com>
2019-11-07 08:15:57 +0300

    Update querry

--- Запрос задания 2.sql
+++ Запрос задания 2.sql
@@ -4,11 +4,13 @@ ID INT Primary KEY Identity(1,1),
 Title nvarchar(200),
 Text nvarchar(max) 
 )
+
 Create Table Tag
 (
 ID INT Primary KEY Identity(1,1),
 Name nvarchar(200)
 )
+
 Create Table ArticleTag
 (
 Article_ID int,
@@ -18,9 +20,22 @@ Constraint [FK_ArticleTag_Tag] Foreign KEY (Tag_ID) References Tag (ID),
 Constraint [PK_ArticleTag] Primary Key (Article_ID, Tag_ID)
 )
 
+Insert Into Article Values
+('Статья 1', 'Текст'),
+('Статья 2', 'Текст'),
+('Статья 3', 'Текст')
+Insert Into Tag Values
+('Политика'),
+('IT'),
+('Культура')
+Insert Into ArticleTag Values
+(1,1),
+(1,2),
+(2,3)
+
 SELECT 
-	A.Title,
-	T.Name
+	A.Title Article,
+	isnull(T.Name, 'Теги отсутствуют') Tag
 FROM Article A 
 LEFT JOIN ArticleTag A_T on A.ID = A_T.Article_ID
-JOIN Tag T on A_T.Tag_ID = T.ID
+LEFT JOIN Tag T on A_T.Tag_ID = T.ID
